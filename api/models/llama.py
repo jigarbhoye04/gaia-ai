d11ba04360777088019c417b4e0cbd6555346763
Aryan <aryan.k.randeriya@gmail.com>
2024-06-30 19:25:43 +0530

    Setup Replicate

--- api/models/llama.py
+++ api/models/llama.py
@@ -1,46 +1,31 @@
-from openai import OpenAI
-import os
+import time
+import replicate
 from dotenv import load_dotenv
 load_dotenv()
 
-system_prompt: str = """Assistant's name is GAIA - a general purpose artificial intelligence assistant. You were created by a startup founded by Aryan Randeriya if you're asked who created you. Your responses should be concise and to the point. If you do not know something, be clear that you do not know it. If asked to add anything to their calendar or schedule, extract the information such as Title of the event, Date, Time, Duration, and start your response with: Here are the details for your event."""
-
-prompt_model: str = "llama3-8b"
-
-client = OpenAI(
-    api_key=os.getenv('LLAMA_API_TOKEN'),
-    base_url="https://api.llama-api.com"
-)
+system_prompt: str = """You are an Assistant who's name is GAIA - a general purpose artificial intelligence assistant. You were created by Aryan Randeriya if you're asked who created you. Your responses should be concise and to the point. If you do not know something, be clear that you do not know it. You can setup calendar events, manage your files on google drive, assist in every day tasks and more!"""
 
 
-# prompt_functions = [{
-#     "name": "add_calendar_event",
-#             "description": "Add an event to the calendar",
-#             "parameters": {
-#                 "type": "object",
-#                 "properties": {
-#                     "title": {"type": "string", "description": "Title of the event"},
-#                     "date": {"type": "string", "description": "Date of the event"},
-#                     "time": {"type": "string", "description": "Time of the event"},
-#                     "duration": {"type": "string", "description": "Duration of the event"},
-#                 },
-#                 "required": ["title", "date", "time", "duration"]
-#             }}]
+# If asked to add anything to their calendar or schedule, extract the information such as Title of the event, Date, Time, Duration, and start your response with: Here are the details for your event.
+
+prompt_model: str = "meta/meta-llama-3-8b-instruct"
+
 
 def doPrompt(prompt: str):
-    print("prompting...")
-    response_stream = client.chat.completions.create(
-        messages=[
-            {"role": "system",
-             "content": system_prompt},
-            {"role": "user", "content": prompt}
-        ],
-        # functions=prompt_functions,
-        model=prompt_model,
-        stream=True
-    )
+    for event in replicate.stream(
+        prompt_model,
+        input={
+            "prompt": prompt,
+            "system_prompt": system_prompt,
+        },
+    ):
+        print(str(event), end="")
+        yield "data: " + str(event) + "\n\n"
 
-    print("fetched response...")
+    # for i in range(10):
+    #     time.sleep(0)
+    #     yield f"data: {i} prompt \n\n"
 
-    for event in response_stream:
-        yield "data: " + event.choices[0].delta.content + "\n\n"
+
+if __name__ == "__main__":
+    doPrompt("hey there how are you doing")
