24d144fa96749c18e2493a9007d9ce6c8ddde7bc
Aryan <aryan.k.randeriya@gmail.com>
2024-07-07 01:33:09 +0530

    Setup cloudflare worker api instead of replicate

--- api/models/llama.py
+++ api/models/llama.py
@@ -1,31 +1,30 @@
-import time
-import replicate
 from dotenv import load_dotenv
 load_dotenv()
+import requests
 
-system_prompt: str = """You are an Assistant who's name is GAIA - a general purpose artificial intelligence assistant. You were created by Aryan Randeriya if you're asked who created you. Your responses should be concise and to the point. If you do not know something, be clear that you do not know it. You can setup calendar events, manage your files on google drive, assist in every day tasks and more!"""
-
-
+system_prompt: str = """You are an Assistant who's name is GAIA - a general purpose artificial intelligence assistant. Your responses should be concise and clear If you're asked who created you then you were created by Aryan Randeriya. Your responses should be concise and to the point. If you do not know something, be clear that you do not know it. You can setup calendar events, manage your files on google drive, assist in every day tasks and more!"""
 # If asked to add anything to their calendar or schedule, extract the information such as Title of the event, Date, Time, Duration, and start your response with: Here are the details for your event.
 
 prompt_model: str = "meta/meta-llama-3-8b-instruct"
-
+import requests
 
 def doPrompt(prompt: str):
-    for event in replicate.stream(
-        prompt_model,
-        input={
-            "prompt": prompt,
-            "system_prompt": system_prompt,
-        },
-    ):
-        print(str(event), end="")
-        yield "data: " + str(event) + "\n\n"
+    url = "https://llm.aryanranderiya1478.workers.dev/"
+    data = {"prompt": prompt}
+    response = requests.post(url, json=data, stream=True)
+    
+    if response.status_code == 200:
+        for line in response.iter_lines():
+            if line:
+                yield line.decode('utf-8') + "\n\n"
+    else:
+        yield "data: Error: Failed to fetch data\n\n"
 
     # for i in range(10):
     #     time.sleep(0)
     #     yield f"data: {i} prompt \n\n"
 
-
 if __name__ == "__main__":
-    doPrompt("hey there how are you doing")
+    prompt = "hey there how are you doing"
+    for event in doPrompt(prompt):
+        print(event)
